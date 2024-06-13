8ba82815b576442d44893c522ab7be9cc09c23fd
Watson <bbcallyou2@gmail.com>
2024-06-13 20:09:08 +0800

    q4 done

--- q4/q4main.js
+++ q4/q4main.js
@@ -1,2 +1,33 @@
 // ### 4. 輸入 n 印出 `1+2-3+4-5+6...n` 的算式與總和
 
+import { rl } from "../tool/readline.js";
+import { checkOdd } from "./q4module.js";
+
+function main(input){
+    rl.question("請輸入隨意值",(input) => {
+
+        let initailNumber = 0;
+        let result = "";
+
+        for(let i = 1; i <= input; i++){
+            if(i === 1){
+                initailNumber = initailNumber + i;
+                result  += `${i}`;
+                continue; 
+            }
+            const isOdd = checkOdd(i);
+            if(isOdd){
+                initailNumber = initailNumber - i;
+                result += `-${i}`;
+                continue;
+            }
+            initailNumber = initailNumber + i;
+            result  += `+${i}`;
+        }
+        console.log(result);
+        console.log(initailNumber);
+        rl.close();
+    })
+}
+
+main();
--- q4/q4module.js
+++ q4/q4module.js
@@ -1,11 +1,36 @@
-// 奇數
-export function isOdd(input){
-    return input % 2 !== 0;
+// export const isZero = input => { 
+//     if(Number
+//         (input) === 0){
+//         throw new Error("請輸入值不為零");
+//     }
+// };
+
+// export const isPositiveInt = input => {
+//     if(!/^(0|[1-9]\d*)$/.test(input)){
+//         throw new Error("請輸入真實年齡");
+//     }
+// }
+
+
+// // 奇數
+// export function isOdd(input){
+//     return input % 2 !== 0;
+// }
+
+// // 偶數
+// export function isEven(input){
+//     return input % 2 === 0;
+// }
+
+export function checkOdd(number){
+    if(number % 2 !== 0){
+        return true;
+    }
+    return false;
 }
 
-// 偶數
-export function isEven(input){
-    return input % 2 === 0;
-}
+
+
+
 
 
