30857f1225180bf1412458879973ba371e85f087
Watson <bbcallyou2@gmail.com>
2024-10-24 19:11:27 +0800

    feat[test] make object.js

--- test/object.js
+++ test/object.js
@@ -10,3 +10,18 @@ let person = {
 
 let letter = "hello"
 
+//嘗試不同object寫法
+
+//1
+let person1 = new Object();
+person1.fristname = "alen";
+person1.lastname  = "wang";
+person1.address   = new Object() 
+person1.address.country   = "Taiwan";
+person1.address.city   = "Tainan";
+person1.address.street   = "zhongzhen";
+
+console.log(person1.address.street)
+
+//2
+
