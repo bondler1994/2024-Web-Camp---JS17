76c3d6035c442b057669de30aecb7df749c68634
Watson <bbcallyou2@gmail.com>
2024-10-24 11:38:25 +0800

    feat[test] make new js file - class & this

---
+++ test/class.js
@@ -0,0 +1,20 @@
+//設定一個情境,設定電話
+
+class Phone {
+    constructor(number, year, is_waterproof) {
+        this.number = number;
+        this.year = year;
+        this.is_waterproof = is_waterproof
+    }
+
+    phone_age() {
+        return 2021 - this.year;
+    }
+}
+
+let phone1 = new Phone("123", 2020, false);
+let phone2 = new Phone("234", 2019, true);
+
+
+console.log(phone1.phone_age());
+console.log(phone2.number);
