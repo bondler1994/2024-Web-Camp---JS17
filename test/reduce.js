bd1f4316a4f12ee65ecd44f3fed665e341740ae1
Watson <bbcallyou2@gmail.com>
2024-11-07 11:42:23 +0800

    feat[test] try reduce

---
+++ test/reduce.js
@@ -0,0 +1,10 @@
+
+function tryToPlus(num){
+    return Array.from(num)
+    console.log(num);
+                // .reduce((num, allNum)=> Number(num +allNum))
+                
+
+}
+
+tryToPlus('12345')
