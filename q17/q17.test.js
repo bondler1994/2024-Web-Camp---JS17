7bb39a9e62570a755b0b8dc84645680d13ad6948
Watson <bbcallyou2@gmail.com>
2024-11-07 10:02:30 +0800

    feat[q17] Chris code review part 1

--- q17/q17.test.js
+++ q17/q17.test.js
@@ -1,21 +1,42 @@
-import { main } from './q17main.js';
-
-test('測試main', () => {
-    const result = main();
-    expect(result).toEqual({
-        a: 253,
-        b: 251,
-        c: 305,
-        d: 191
-    });
-});
-
+import { q17 } from "./q17module.js";
+// test('測試main', () => {
+//     const result = main();
+//     expect(result).toEqual({
+//         a: 253,
+//         b: 251,
+//         c: 305,
+//         d: 191
+//     });
+// });
 
 
-describe('測試borda count,使用AAA', () => {
-    test('測試計算是否正確', () => {
-        
+
+describe('測試Q17,使用AAA', () => {
+    test('測試', () => {
         // Arrange
+        //建立分數
+        const score = [4, 3, 2, 1]
+
+
+        //建立選民人數
+        const vote = [51, 5, 23, 21]
+
+        //建立候選人
+        // const candidates = [
+        //     //col
+        //     ["a", "c", "b", "d"],//4row
+        //     ["c", "b", "c", "c"],//3
+        //     ["b", "d", "d", "b"],//2
+        //     ["d", "a", "a", "a"],//1
+        // ]
+        const candidates2 = [
+            //col
+            ['a', 'c', 'b', 'd'],
+            ['c', 'b', 'd', 'a'],
+            ['b', 'c', 'd', 'a'],
+            ['d', 'c', 'b', 'a']
+        ]
+
         // 預期分數：根據給定的 score 和 vote 計算出的結果
         const expectedScores = {
             a: 253,
@@ -26,7 +47,94 @@ describe('測試borda count,使用AAA', () => {
 
         // Act
         // 執行 main 函數，獲得候選人的分數
-        const result = main();
+        const result = q17(candidates2, score, vote);
+
+        // Assert
+        // 驗證結果是否符合預期
+        expect(result).toEqual(expectedScores);
+    });
+
+    test('測試2', () => {
+        // Arrange
+
+
+        //建立選民人數
+        const vote = [51, 5, 23, 21, 100]
+
+        //建立候選人
+        // const candidates = [
+        //     //col
+        //     ["a", "c", "b", "d"],//4row
+        //     ["c", "b", "c", "c"],//3
+        //     ["b", "d", "d", "b"],//2
+        //     ["d", "a", "a", "a"],//1
+        // ]
+        const candidates2 = [
+            //col
+            ['a', 'c', 'b', 'd'],
+            ['c', 'b', 'd', 'a'],
+            ['b', 'c', 'd', 'a'],
+            ['d', 'c', 'b', 'a'],
+            ['a', 'b', 'd', 'c']
+        ]
+        //建立分數
+        const score = [4, 3, 2, 1]
+
+        // 預期分數：根據給定的 score 和 vote 計算出的結果
+        const expectedScores = {
+            a: 653,
+            b: 551,
+            c: 405,
+            d: 391
+        };
+
+        // Act
+        // 執行 main 函數，獲得候選人的分數
+        const result = q17(candidates2, score, vote);
+
+        // Assert
+        // 驗證結果是否符合預期
+        expect(result).toEqual(expectedScores);
+    });
+
+    test.only('測試2', () => {
+        // Arrange
+
+
+        //建立選民人數
+        const vote = [51, 5, 23, 21, 100]
+
+        //建立候選人
+        // const candidates = [
+        //     //col
+        //     ["a", "c", "b", "d"],//4row
+        //     ["c", "b", "c", "c"],//3
+        //     ["b", "d", "d", "b"],//2
+        //     ["d", "a", "a", "a"],//1
+        // ]
+        const candidates2 = [
+            //col
+            ['a', 'c', 'b', 'd', 'e'],
+            ['c', 'b', 'd', 'e', 'a'],
+            ['b', 'c', 'e', 'd', 'a'],
+            ['d', 'e', 'c', 'b', 'a'],
+            ['a', 'b', 'd', 'e', 'c']
+        ]
+        //建立分數
+        const score = [5, 4, 3, 2, 1]
+
+        // 預期分數：根據給定的 score 和 vote 計算出的結果
+        const expectedScores = {
+            a: 653,
+            b: 551,
+            c: 405,
+            d: 391,
+            e: 0
+        };
+
+        // Act
+        // 執行 main 函數，獲得候選人的分數
+        const result = q17(candidates2, score, vote);
 
         // Assert
         // 驗證結果是否符合預期
--- q17/q17main.js
+++ q17/q17main.js
@@ -8,19 +8,16 @@
 // 3rd | 2  | 4  | 4  | 2
 // 4th | 4  | 1  | 1  | 1
 
-
-
-
-
 //我需要做什麼
 //目前已知投票人數,投票分數,要怎麼做才可以
 //建立分數,建立abcd為候選人
 //要作到顯示abcd在每局得牌位？
 //讓使用者輸入abcd牌位
 //最後列印出abcd得分數以及每局分數？
+import { q17 } from "./q17module.js"  // ES module
+// const q17 = require('./q17module.js') // commonJS
 
-
-export function main() {
+function main() {
 
     //建立分數
     const score = [4, 3, 2, 1]
@@ -30,51 +27,38 @@ export function main() {
     const vote = [51, 5, 23, 21]
 
     //建立候選人
-    const candidates = [
+    // const candidates = [
+    //     //col
+    //     ["a", "c", "b", "d"],//4row
+    //     ["c", "b", "c", "c"],//3
+    //     ["b", "d", "d", "b"],//2
+    //     ["d", "a", "a", "a"],//1
+    // ]
+    const candidates2 = [
         //col
-        ["a", "c", "b", "d"],//4row
-        ["c", "b", "c", "c"],//3
-        ["b", "d", "d", "b"],//2
-        ["d", "a", "a", "a"],//1
+        ['a', 'c', 'b', 'd'],
+        ['c', 'b', 'd', 'a'],
+        ['b', 'c', 'd', 'a'],
+        ['d', 'c', 'b', 'a']
     ]
-
-    //建立計分系統,用來儲存分數,之後跑迴圈數據存取在這裡,最後要列印出它
-    const candidatesScore = {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 0
-    }
-
-    // candidates.forEach((row, rowIndex)=>{
-    //     row.forEach((col, colIndex)=>{
-    //         candidatesScore += (score*rowIndex ) * (vote * colIndex)
+    // candidates2.forEach((row, rowIndex) => {
+    //     const row2 = row.map((col, colIndex) => {
+    //         return candidates2[colIndex][rowIndex]
     //     })
+    //     console.log(row2)
     // })
 
-    //運用forEach（陣列專用method），然後跑候選人二維陣列，candidates.forEach得row 是指順位 也代表1st 2nd 3rd 4th得整列 陣列上到下，rowindex是代表給陣列順位編號 0,1,2,3，
-    //row.forEach就是跑row上往下走，然後col是指a,c,b,d，colIndex代表陣列內容 賦予編號0,1,2,3
-    //最後
-    candidates.forEach((row, rowIndex)=>{
-        // console.log(row)
-        // console.log(rowIndex)
-
-        row.forEach((col, colIndex)=>{
-            // console.log(col)
-            // console.log(colIndex)
-            candidatesScore[col] += score[rowIndex] * vote[colIndex]
-            // console.log(vote[colIndex])
-            // console.log(score[rowIndex] * vote[colIndex])
-            
-        })
-    })
+    //建立計分系統,用來儲存分數,之後跑迴圈數據存取在這裡,最後要列印出它
+    const ans = q17(candidates2, score, vote)
+    console.log(ans);
 
     // console.log("所有候選人分數各別是",candidatesScore)
 
-    return("所有候選人分數各別是",candidatesScore)
+    // return ("所有候選人分數各別是", candidatesScore)
     // a = 51*4 + 5*1 + 23*1 + 21*1
 
 }
 
-const ans = main()
-console.log(ans);
+main()
+
+
